c74a756bc8fb15e00b9b766bb5ea4d203dfd82da
Sam Ruby <rubys@intertwingly.net>
2020-04-26 17:22:14 -0400

    non agenda items are blank, not missing

--- src/client/elements/colorize.js
+++ src/client/elements/colorize.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 function Colorize({ item, minutes, pending, drafts, children }) {
 
   let color = (() => {
-    if (!item) {
+    if (!item?.attach) {
       return "blank"
     } if (flagged()) {
       return "commented"
