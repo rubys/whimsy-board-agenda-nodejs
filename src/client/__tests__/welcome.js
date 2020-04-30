dd85e6e141e25cc5558597a24b8d19f478d89107
Sam Ruby <rubys@intertwingly.net>
2020-04-30 17:43:42 -0400

    switch to test renderer

--- src/client/__tests__/welcome.js
+++ src/client/__tests__/welcome.js
@@ -4,20 +4,18 @@ import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 import store from '..//store';
 import Router from '../router.js';
+import TestRenderer from 'react-test-renderer';
 
 test('renders welcome splash screen', () => {
   const context = {};
 
-  const { getByText } = render(<>
-    <base href="/"/>
+  let root = TestRenderer.create(<>
     <Provider store={store}>
       <StaticRouter location='/' context={context}>
         <Router />
       </StaticRouter>
     </Provider>
-  </>);
+  </>).root;
 
-  const progressIndicator = getByText(/Fetching board agenda/i);
-
-  expect(progressIndicator).toBeInTheDocument();
+  expect(root.findByType('p').children[0]).toBe("Fetching board agenda...");
 });
