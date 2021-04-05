e16924d53326e5219e1d9388a480617090ae93ba
Sam Ruby <rubys@intertwingly.net>
2021-04-05 09:05:54 -0400

    footer => hook

--- src/client/layout/footer.js
+++ src/client/layout/footer.js
@@ -17,180 +17,172 @@ function mapStateToProps(state) {
 // (last executive officer <-> first flagged/unapproved/missing &&
 //  last flagged/unapproved/missing <-> first Special order)
 //
-class Footer extends React.Component {
-  render() {
-    let item = this.props.item || this.props;
-    let { traversal, meetingDay } = this.props;
+function Footer(props) {
+  let item = props.item || props;
+  let { traversal, meetingDay } = props;
 
-    let color = this.props.color || item?.status?.color || 'blank'
+  let color = props.color || item?.status?.color || 'blank'
 
-    return <footer className={`fixed-bottom navbar ${color}`}>
-      <PrevLink item={item} agenda={this.props.agenda} traversal={traversal} meetingDay={meetingDay} />
+  return <footer className={`fixed-bottom navbar ${color}`}>
+    <PrevLink item={item} agenda={props.agenda} traversal={traversal} meetingDay={meetingDay} />
 
-      <span>{this.props.buttons ? this.props.buttons.map(button => {
-        let props;
+    <span>{props.buttons ? props.buttons.map(button => {
+      if (button.text) {
+        let bprops = { ...button.attrs, key: button.text };
 
-        if (button.text) {
-          props = { ...button.attrs, key: button.text };
+        if (button.attrs.class) {
+          bprops.className = button.attrs.class.split(" ");
+          delete bprops.class;
+        };
 
-          if (button.attrs.class) {
-            props.className = button.attrs.class.split(" ");
-            delete props.class;
-          };
+        return React.createElement("button", bprops, button.text)
+      } else if (button.type) {
+        let type = button.type;
+        if (type.WrappedComponent) type = type.WrappedComponent;
+        return React.createElement(button.type, { ...button.attrs, key: type.name })
+      }
 
-          return React.createElement("button", props, button.text)
-        } else if (button.type) {
-          let type = button.type;
-          if (type.WrappedComponent) type = type.WrappedComponent;
-          return React.createElement(button.type, { ...button.attrs, key: type.name })
-        }
+      return null
+    }) : null}</span>
 
-        return null
-      }) : null}</span>
-
-      <NextLink item={item} agenda={this.props.agenda} traversal={traversal} meetingDay={meetingDay} />
-    </footer>
-  }
+    <NextLink item={item} agenda={props.agenda} traversal={traversal} meetingDay={meetingDay} />
+  </footer>
 };
 
 /* eslint-disable jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content */
 
-class PrevLink extends React.Component {
-  render() {
-    let { agenda, item, traversal, meetingDay } = this.props;
+function PrevLink(props) {
+  let { agenda, item, traversal, meetingDay } = props;
 
-    let link = item?.prev;
-    if (link && !link.title) link = agenda[link];
-    let prefix = "/";
+  let link = item?.prev;
+  if (link && !link.title) link = agenda[link];
+  let prefix = "/";
 
-    if (traversal === "queue") {
-      while (link && !link.status.ready_for_review) {
-        link = agenda[link.prev]
-      };
-
-      if (link) {
-        prefix = "/queue/";
-      } else {
-        link = link || { href: "queue", title: "Queue" };
-      }
-    } else if (traversal === "shepherd") {
-      while (link && link.shepherd !== item.shepherd) {
-        link = agenda[link.prev]
-      };
-
-      if (link) {
-        prefix = "/shepherd/queue/";
-      } else {
-        link = {
-          href: `shepherd/${item.shepherd}`,
-          title: "Shepherd"
-        }
-      }
-    } else if (traversal === "flagged") {
-      prefix = "/flagged/";
-
-      while (link && link.status.skippable) {
-        if (/^\d+[A-Z]*$/m.test(link.attach)) {
-          prefix = "/";
-          break
-        } else {
-          link = agenda[link.prev]
-        }
-      };
-
-      if (!link) {
-        prefix = "/";
-        link = link || { href: "flagged", title: "Flagged" }
-      }
-    } else if (meetingDay && link && /^\d/m.test(item.attach) && /^[A-Z]/m.test(link.attach)) {
-      while (link && link.status.skippable && /^([A-Z]|\d+$)/m.test(link.attach)) {
-        link = agenda[link.prev]
-      };
-
-      prefix = "/flagged/"
+  if (traversal === "queue") {
+    while (link && !link.status.ready_for_review) {
+      link = agenda[link.prev]
     };
 
     if (link) {
-      if (!/^([A-Z]|\d+$)/m.test(link.attach)) prefix = "/";
-
-      let color = link?.status?.color || 'blank';
-
-      return prefix === '/' && link.href.startsWith('../')
-        ? <a aria-label='prev' className={`navbar-brand backlink ${color}`} rel="prev" href={link.href}>{link.title}</a>
-        : <Link aria-label='prev' className={`navbar-brand backlink ${color}`} rel="prev" to={`${prefix}${link.href}`}>{link.title}</Link>
-    } else if (item?.prev || item?.next) {
-      return <a className="navbar-brand" />
+      prefix = "/queue/";
     } else {
-      return null
+      link = link || { href: "queue", title: "Queue" };
     }
+  } else if (traversal === "shepherd") {
+    while (link && link.shepherd !== item.shepherd) {
+      link = agenda[link.prev]
+    };
+
+    if (link) {
+      prefix = "/shepherd/queue/";
+    } else {
+      link = {
+        href: `shepherd/${item.shepherd}`,
+        title: "Shepherd"
+      }
+    }
+  } else if (traversal === "flagged") {
+    prefix = "/flagged/";
+
+    while (link && link.status.skippable) {
+      if (/^\d+[A-Z]*$/m.test(link.attach)) {
+        prefix = "/";
+        break
+      } else {
+        link = agenda[link.prev]
+      }
+    };
+
+    if (!link) {
+      prefix = "/";
+      link = link || { href: "flagged", title: "Flagged" }
+    }
+  } else if (meetingDay && link && /^\d/m.test(item.attach) && /^[A-Z]/m.test(link.attach)) {
+    while (link && link.status.skippable && /^([A-Z]|\d+$)/m.test(link.attach)) {
+      link = agenda[link.prev]
+    };
+
+    prefix = "/flagged/"
+  };
+
+  if (link) {
+    if (!/^([A-Z]|\d+$)/m.test(link.attach)) prefix = "/";
+
+    let color = link?.status?.color || 'blank';
+
+    return prefix === '/' && link.href.startsWith('../')
+      ? <a aria-label='prev' className={`navbar-brand backlink ${color}`} rel="prev" href={link.href}>{link.title}</a>
+      : <Link aria-label='prev' className={`navbar-brand backlink ${color}`} rel="prev" to={`${prefix}${link.href}`}>{link.title}</Link>
+  } else if (item?.prev || item?.next) {
+    return <a className="navbar-brand" />
+  } else {
+    return null
   }
 }
 
-class NextLink extends React.Component {
-  render() {
-    let { agenda, item, traversal, meetingDay } = this.props;
+function NextLink(props) {
+  let { agenda, item, traversal, meetingDay } = props;
 
-    let link = item?.next;
-    if (link && !link.title) link = agenda[link];
-    let prefix = '/';
+  let link = item?.next;
+  if (link && !link.title) link = agenda[link];
+  let prefix = '/';
 
-    if (traversal === "queue") {
-      while (link && !link.status.ready_for_review) {
-        link = agenda[link.next]
-      };
-
-      if (link) {
-        prefix = '/queue/';
-      } else {
-        link = { href: "queue", title: "Queue" };
-      }
-    } else if (traversal === "shepherd") {
-      while (link && link.shepherd !== item.shepherd) {
-        link = agenda[link.next]
-      };
-
-      if (link) {
-        prefix = "/shepherd/queue/";
-      } else {
-        link = {
-          href: `shepherd/${item.shepherd}`,
-          title: "Shepherd"
-        }
-      }
-    } else if (traversal === "flagged") {
-      prefix = "/flagged/";
-
-      while (link && link.status.skippable) {
-        if (meetingDay && !/^(\d+|[A-Z]+)$/m.test(link.attach)) {
-          prefix = "/";
-          break
-        } else {
-          link = agenda[link.next]
-        }
-      };
-
-      link = link || { href: "flagged", title: "Flagged" }
-    } else if (meetingDay && link && /^\d/m.test(item.attach) && /^[A-Z]/m.test(link.attach)) {
-      while (link && link.status.skippable && /^([A-Z]|\d+$)/m.test(link.attach)) {
-        link = agenda[link.next]
-      };
-
-      prefix = "/flagged/"
+  if (traversal === "queue") {
+    while (link && !link.status.ready_for_review) {
+      link = agenda[link.next]
     };
 
     if (link) {
-      if (!/^([A-Z]|\d+$)/m.test(link.attach)) prefix = "/";
-
-      let color = link?.status?.color || 'blank';
-
-      return prefix === '/' && link.href.startsWith('../')
-        ? <a aria-label='next' className={`navbar-brand nextlink ${color}`} rel="next" href={link.href}>{link.title}</a>
-        : <Link aria-label='next' className={`navbar-brand nextlink ${color}`} rel="next" to={`${prefix}${link.href}`}>{link.title}</Link>
-    } else if (item?.prev || item?.next) {
-      return <a className="navbar-brand" />
+      prefix = '/queue/';
     } else {
-      return null
+      link = { href: "queue", title: "Queue" };
     }
+  } else if (traversal === "shepherd") {
+    while (link && link.shepherd !== item.shepherd) {
+      link = agenda[link.next]
+    };
+
+    if (link) {
+      prefix = "/shepherd/queue/";
+    } else {
+      link = {
+        href: `shepherd/${item.shepherd}`,
+        title: "Shepherd"
+      }
+    }
+  } else if (traversal === "flagged") {
+    prefix = "/flagged/";
+
+    while (link && link.status.skippable) {
+      if (meetingDay && !/^(\d+|[A-Z]+)$/m.test(link.attach)) {
+        prefix = "/";
+        break
+      } else {
+        link = agenda[link.next]
+      }
+    };
+
+    link = link || { href: "flagged", title: "Flagged" }
+  } else if (meetingDay && link && /^\d/m.test(item.attach) && /^[A-Z]/m.test(link.attach)) {
+    while (link && link.status.skippable && /^([A-Z]|\d+$)/m.test(link.attach)) {
+      link = agenda[link.next]
+    };
+
+    prefix = "/flagged/"
+  };
+
+  if (link) {
+    if (!/^([A-Z]|\d+$)/m.test(link.attach)) prefix = "/";
+
+    let color = link?.status?.color || 'blank';
+
+    return prefix === '/' && link.href.startsWith('../')
+      ? <a aria-label='next' className={`navbar-brand nextlink ${color}`} rel="next" href={link.href}>{link.title}</a>
+      : <Link aria-label='next' className={`navbar-brand nextlink ${color}`} rel="next" to={`${prefix}${link.href}`}>{link.title}</Link>
+  } else if (item?.prev || item?.next) {
+    return <a className="navbar-brand" />
+  } else {
+    return null
   }
 }
 
